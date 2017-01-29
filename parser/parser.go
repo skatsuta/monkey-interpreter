270c2a8b8d507321393cd1b32cdc0b16f4931a21
Soshi Katsuta <soshi.katsuta@gmail.com>
2017-01-29 14:45:50 +0900

    parser: add support to parse grouped expressions
    Signed-off-by: Soshi Katsuta <soshi.katsuta@gmail.com>

--- parser/parser.go
+++ parser/parser.go
@@ -70,6 +70,7 @@ func New(l lexer.Lexer) *Parser {
 	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 	p.registerPrefix(token.TRUE, p.parseBoolean)
 	p.registerPrefix(token.FALSE, p.parseBoolean)
+	p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
 
 	p.registerInfix(token.PLUS, p.parseInfixExpression)
 	p.registerInfix(token.MINUS, p.parseInfixExpression)
@@ -299,4 +300,16 @@ func (p *Parser) parseBoolean() ast.Expression {
 		Token: p.curToken,
 		Value: p.curTokenIs(token.TRUE),
 	}
+}
+
+func (p *Parser) parseGroupedExpression() ast.Expression {
+	p.nextToken()
+
+	expr := p.parseExpression(LOWEST)
+
+	if !p.expectPeek(token.RPAREN) {
+		return nil
+	}
+
+	return expr
 }
