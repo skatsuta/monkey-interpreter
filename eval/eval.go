628f5a196385f7a90c74f45af197a7e88686133a
Soshi Katsuta <soshi.katsuta@gmail.com>
2017-01-31 01:43:46 +0900

    eval: add support to evaluate boolean literals
    Signed-off-by: Soshi Katsuta <soshi.katsuta@gmail.com>

--- eval/eval.go
+++ eval/eval.go
@@ -16,6 +16,8 @@ func Eval(node ast.Node) object.Object {
 	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return &object.Boolean{Value: node.Value}
 	default:
 		return nil
 	}
--- eval/eval_test.go
+++ eval/eval_test.go
@@ -8,21 +8,6 @@ import (
 	"github.com/skatsuta/monkey-interpreter/parser"
 )
 
-func TestEvalIntegerExpression(t *testing.T) {
-	tests := []struct {
-		input    string
-		expected int64
-	}{
-		{"5", 5},
-		{"10", 10},
-	}
-
-	for _, tt := range tests {
-		evaluated := testEval(tt.input)
-		testIntegerObject(t, evaluated, tt.expected)
-	}
-}
-
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
@@ -39,5 +24,46 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) {
 
 	if result.Value != expected {
 		t.Errorf("object has wrong value. want=%d, got=%d", expected, result.Value)
+	}
+}
+
+func TestEvalIntegerExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) {
+	result, ok := obj.(*object.Boolean)
+	if !ok {
+		t.Fatalf("object is not *object.Boolean. got=%T (%+v)", obj, obj)
+	}
+
+	if result.Value != expected {
+		t.Errorf("object has wrong value. want=%t, got=%t", expected, result.Value)
+	}
+}
+
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
 	}
 }
