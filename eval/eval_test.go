7424d411a65fca65b349e549190ea6d781fd98bd
Soshi Katsuta <soshi.katsuta@gmail.com>
2017-02-04 00:08:12 +0900

    fixup! eval: do not return
    Signed-off-by: Soshi Katsuta <soshi.katsuta@gmail.com>

--- eval/eval_test.go
+++ eval/eval_test.go
@@ -221,8 +221,8 @@ func TestLetStatements(t *testing.T) {
 		input    string
 		expected int64
 	}{
-		{"let a = 5;", 5},
-		{"let a = 5 * 5;", 25},
+		{"let a = 5; a;", 5},
+		{"let a = 5 * 5; a;", 25},
 		{"let a = 5; let b = a; b;", 5},
 		{"let a = 5; let b = a; let c = a + b + 5; c;", 15},
 	}
