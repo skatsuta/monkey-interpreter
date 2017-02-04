a3a387d9c55766780cf3b83eb14d931dbfed28b7
Soshi Katsuta <soshi.katsuta@gmail.com>
2017-02-05 01:05:28 +0900

    main: run a script when a file name is passed as an argument into the interpreter
    Signed-off-by: Soshi Katsuta <soshi.katsuta@gmail.com>

--- main.go
+++ main.go
@@ -2,13 +2,51 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 
+	"github.com/skatsuta/monkey-interpreter/eval"
+	"github.com/skatsuta/monkey-interpreter/lexer"
+	"github.com/skatsuta/monkey-interpreter/object"
+	"github.com/skatsuta/monkey-interpreter/parser"
 	"github.com/skatsuta/monkey-interpreter/repl"
 )
 
 func main() {
-	fmt.Printf("This is the Monkey programming language!\n")
-	fmt.Printf("Feel free to type in commands\n")
-	repl.Start(os.Stdin, os.Stdout)
+	// Start Monkey REPL
+	if len(os.Args) == 1 {
+		fmt.Println("This is the Monkey programming language!")
+		fmt.Println("Feel free to type in commands")
+		repl.Start(os.Stdin, os.Stdout)
+		return
+	}
+
+	// Run a Monkey script
+	runProgram(os.Args[1])
+}
+
+func runProgram(filename string) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not read %s: %v\n", os.Args[1], err)
+		os.Exit(1)
+	}
+
+	p := parser.New(lexer.New(string(data)))
+	program := p.ParseProgram()
+	if len(p.Errors()) > 0 {
+		for _, e := range p.Errors() {
+			fmt.Fprintln(os.Stderr, e)
+		}
+		os.Exit(1)
+	}
+
+	env := object.NewEnvironment()
+	evaluated := eval.Eval(program, env)
+	if evaluated == nil {
+		return
+	}
+
+	io.WriteString(os.Stdout, evaluated.Inspect()+"\n")
 }
