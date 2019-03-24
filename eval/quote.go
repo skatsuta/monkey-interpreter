d282572e31d080ea2c279a69119eb8635760c7b8
Soshi Katsuta <soshi.katsuta@gmail.com>
2019-03-24 18:11:17 +0900

    eval: define constants for `quote` and `unquote` function names

--- eval/quote.go
+++ eval/quote.go
@@ -5,6 +5,13 @@ import (
 	"github.com/skatsuta/monkey-interpreter/object"
 )
 
+const (
+	// FuncNameQuote is a name for quote function.
+	FuncNameQuote = "quote"
+	// FuncNameUnquote is a name for unquote function.
+	FuncNameUnquote = "unquote"
+)
+
 func quote(node ast.Node) object.Object {
 	return &object.Quote{Node: node}
 }
